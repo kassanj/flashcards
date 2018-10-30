eee3d6104cec8e205625c701121dfe26cfa3dadf
kassanj <kassandra@whereby.us>
2018-10-30 10:35:37 -0700

    Add components

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Deck from './components/Deck'
+import Card from './components/Card'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Card />
+        <Deck />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
---
+++ components/AddDeck.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+
+class AddDeck extends Component {
+
+  render() {
+
+    return (
+      <View>
+        <Text>Add Deck</Text>
+      </View>
+    )
+  }
+}
+
+export default AddDeck
