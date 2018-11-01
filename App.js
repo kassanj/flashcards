592e283662e00d3b41a315dc8e5b040d583346df
kassanj <kassandra@whereby.us>
2018-10-31 17:35:02 -0700

    fix: restructure mainNavigation

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { View, StyleSheet, Text, Platform, StatusBar } from 'react-native';
+import { View, StyleSheet, Text, Platform, StatusBar, TouchableOpacity } from 'react-native';
+
 import Deck from './components/Deck';
 import Card from './components/Card';
 import AddDeck from './components/AddDeck';
 import AddCard from './components/AddCard';
 import DeckList from './components/DeckList';
+import Quiz from './components/Quiz';
 import Home from './components/Home';
+
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator, navigationOptions } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { Constants } from 'expo';
+
+import { Plus } from './utils/styles';
 
 function AppStatusBar ({backgroundColor, ...props}) {
   return (
@@ -21,66 +26,38 @@ function AppStatusBar ({backgroundColor, ...props}) {
   )
 }
 
-const Tabs = createBottomTabNavigator({
-  DeckList: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-home' size={30} color={tintColor} />
-    },
-  },
-  AddDeck: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'Add Deck',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    },
-  },
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? '#008080' : '#FFF',
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? '#FFF' : '#008080',
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
-
 const MainNavigator = createStackNavigator({
-  home: {
-    screen: Tabs,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  Card: {
-    screen: Card,
-    navigationOptions: ({ navigation }) => ({
-      headerTintColor: '#FFF',
-      headerStyle: {
-        backgroundColor: '#008080',
-      },
-    }),
+  DeckList: {
+    screen: DeckList
   },
   Deck: {
-    screen: Deck,
-    navigationOptions: ({ navigation }) => ({
-      headerTintColor: '#FFF',
-      headerStyle: {
-        backgroundColor: '#008080',
-      },
-    }),
+    screen: Deck
   },
+  AddDeck: {
+    screen: AddDeck
+  },
+  AddCard: {
+    screen: AddCard
+  },
+  Quiz: {
+    screen: Quiz
+  },
+  initialRouteName: 'DeckList'
+});
+
+
+// Navbar, with + to add new deck
+DeckList.navigationOptions = ({ navigation }) => ({
+  title: 'Decks',
+  headerRight: (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('AddDeck');
+      }}
+    >
+      <Plus>+</Plus>
+    </TouchableOpacity>
+  )
 });
 
 
@@ -90,7 +67,7 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <AppStatusBar />
-          <Tabs />
+          <MainNavigator />
         </View>
       </Provider>
     );
