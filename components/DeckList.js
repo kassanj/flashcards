6f080cba0a386ba801eed8ad01ad7a10ff38b51b
kassanj <kassandra@whereby.us>
2018-10-31 17:36:42 -0700

    add Flatview to loop title of decks

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
+import { connect } from 'react-redux';
 import navigationOptions from 'react-navigation';
-
+import { getDecks } from '../utils/api';
+import { receiveDecks } from '../actions';
 
 
 class DeckList extends Component {
 
+  componentDidMount() {
+    getDecks().then(decks =>
+      this.props.dispatch(receiveDecks(JSON.parse(decks)))
+    );
+  }
 
   render() {
 
@@ -13,9 +20,28 @@ class DeckList extends Component {
       <View>
         // Loop through Decks
         <Text>Deck List</Text>
+          <FlatList
+            data={Object.values(this.props.allDecks)}
+            renderItem={({item}) =>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.navigate('Deck', {
+                  title: item.title
+                });
+              }}
+            >
+              <Text>{item.title}</Text>
+           </TouchableOpacity>
+           }
+           keyExtractor={item => item.title}
+          />
       </View>
     )
   }
 }
 
-export default DeckList
+const mapStateToProps = state => ({
+  allDecks: state.decks || []
+});
+
+export default connect(mapStateToProps)(DeckList);
