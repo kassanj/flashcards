75cfafd4c709b115dcbe8012cfd9fe1b0e644725
kassanj <kassandra@whereby.us>
2018-11-02 10:59:01 -0700

    feat: add Quiz functionality and styling

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,24 +1,126 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import { PageContainer, Title, TitleBox, Subtext, Button, ButtonTitle } from '../utils/styles';
+import navigationOptions from 'react-navigation'
+import {
+  PageContainer,
+  Title,
+  QuestionTitle,
+  AnswerTitle,
+  TitleBox,
+  SubText,
+  Button,
+  ButtonTitle,
+  QuizPage,
+  QuizTitle,
+  QuizEnumeration,
+  ScoreResult,
+} from '../utils/styles';
 
 class Quiz extends Component {
 
-  render() {
-    return (
-      <PageContainer>
-        // The Quiz view starts with a question from the selected deck.
-        // The question is displayed, along with a button to show the answer.
-        // Pressing the 'Show Answer' button displays the answer.
-        // Buttons are included to allow the student to mark their guess as 'Correct' or 'Incorrect'
-        // The view displays the number of questions remaining.
-        // When the last question is answered, a score is displayed. This can be displayed as a percentage of correct answers or just the number of questions answered correctly.
-        // When the score is displayed, buttons are displayed to either start the quiz over or go back to the Individual Deck view.
-        // Both the 'Restart Quiz' and 'Back to Deck' buttons route correctly to their respective views.
-        <Title>Quiz</Title>
-      </PageContainer>
-    )
-  }
+    state = {
+      page: 0,
+      showAnswer: false,
+      score: 0
+    };
+
+    static navigationOptions = {
+      Quiz: 'Quiz'
+    };
+
+    render() {
+
+      const { score, page, displayAnswer } = this.state;
+      const { params } = this.props.navigation.state;
+      const item = params ? params.item : null;
+      const pages = item.questions === undefined ? 0 : item.questions.length;
+
+      return (
+        <View>
+          {page < pages ? (
+            <PageContainer>
+              <QuizEnumeration>
+                {page + 1}/{pages}
+              </QuizEnumeration>
+              <QuestionTitle>Q: {item.questions[page].question}</QuestionTitle>
+              {displayAnswer === true ? (
+                <PageContainer>
+                  <AnswerTitle>A: {item.questions[page].answer}</AnswerTitle>
+                  <Button
+                    onPress={() => {
+                      this.setState({
+                        score: score + 1,
+                        page: page + 1,
+                        displayAnswer: false
+                      });
+                    }}
+                  >
+                    <ButtonTitle>Correct</ButtonTitle>
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      this.setState({
+                        page: page + 1,
+                        displayAnswer: false,
+                      });
+                    }}
+                  >
+                    <ButtonTitle>Incorrect</ButtonTitle>
+                  </Button>
+                </PageContainer>
+              ) : (
+                <PageContainer>
+                  <QuizTitle />
+                  <Button
+                    onPress={() => {
+                      this.setState({ displayAnswer: true });
+                    }}
+                  >
+                    <ButtonTitle>Show Answer</ButtonTitle>
+                  </Button>
+                </PageContainer>
+              )}
+              <QuizEnumeration>{pages - page - 1} remaining</QuizEnumeration>
+            </PageContainer>
+          ) : (
+            <PageContainer>
+              <ScoreResult>You scored {Math.round(score / pages * 100)}%</ScoreResult>
+              <QuizEnumeration>
+                ({score} correct from {pages}{' '}
+                {pages === 1 ? <SubText>question</SubText> : <SubText>questions</SubText>})
+              </QuizEnumeration>
+              <Button
+                onPress={() => {
+                  this.setState({
+                    score: 0,
+                    page: 0,
+                    displayAnswer: false
+                  });
+                }}
+              >
+                <ButtonTitle>Restart Quiz</ButtonTitle>
+              </Button>
+              <Button
+                onPress={() => {
+                  this.props.navigation.navigate('Deck', {
+                    title: item.title
+                  });
+                }}
+              >
+                <ButtonTitle>Back to Deck</ButtonTitle>
+              </Button>
+              <Button
+                onPress={() => {
+                  this.props.navigation.navigate('DeckList');
+                }}
+              >
+                <ButtonTitle>View all Decks</ButtonTitle>
+              </Button>
+            </PageContainer>
+          )}
+        </View>
+      );
+    }
 }
 
 export default Quiz
--- utils/styles.js
+++ utils/styles.js
@@ -27,11 +27,26 @@ export const Title = styled.Text`
   text-align: center;
 `;
 
+export const QuestionTitle = styled.Text`
+  color: black;
+  font-size: 20;
+  text-align: center;
+  margin-top: 30;
+`;
+
+export const AnswerTitle = styled.Text`
+  color: black;
+  font-size: 20;
+  text-align: center;
+  margin-top: 20;
+  margin-bottom: 20;
+`;
+
 export const TitleBox = styled.View`
   margin: 30px;
 `;
 
-export const Subtext = styled.Text`
+export const SubText = styled.Text`
   margin: 12px;
   color: gray;
   text-align: center;
@@ -63,4 +78,37 @@ export const Input = styled.TextInput`
   border-width: 1;
   margin: 10px;
   padding: 10px;
+`;
+
+export const QuizEnumeration = styled.Text`
+  color: grey;
+  font-size: 14;
+  text-align: center;
+  margin-bottom: 15;
+`;
+
+export const Enumeration = styled.Text`
+  color: grey;
+  font-size: 14;
+  text-align: center;
+`;
+
+export const QuizPage = styled.View`
+  flex: 1;
+  padding: 2px;
+`;
+
+export const QuizTitle = styled.Text`
+  color: black;
+  font-size: 18;
+  text-align: center;
+  margin-top: 30;
+`;
+
+export const ScoreResult = styled.Text`
+  color: black;
+  font-size: 24;
+  text-align: center;
+  margin-top: 30;
+  margin-bottom: 20;
 `;
